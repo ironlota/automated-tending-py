2acbdb294e60b2911f07f259f293ca32be808885
Ray Andrew <raydreww@gmail.com>
2019-11-04 14:33:46 +0700

    [WIP] add testcase for finger movement

--- tests/mechanisms/test_finger_movement.py
+++ tests/mechanisms/test_finger_movement.py
@@ -1,7 +1,28 @@
-import unittest
 import pytest
+from unittest import mock
 
 import qtpy
 
-def test_x():
-    assert 1 == 1
+from emmerich_atm.mechanisms.finger_positioning import FingerPositioning
+from emmerich_atm.config import Config
+
+from ..utils.mock_config import config_str
+
+class TestFingerPositioning:
+    @pytest.fixture(autouse=True)
+    def setup_method(self, tmpdir):
+        self.yaml = tmpdir.mkdir('config').join('config.yaml')
+        self.yaml.write(config_str)
+        self.config = Config(filename=self.yaml).config
+        
+        with mock.patch('emmerich_atm.mechanisms.finger_positioning.FingerPositioning.__init__', mock.Mock(return_value=None)):
+            self.finger_positioning = FingerPositioning()
+            self.finger_positioning.config = self.config
+            with mock.patch('gpiozero.OutputDevice', autospec=True) as mock_output_device:
+                self.finger_positioning.sleep = mock_output_device.return_value
+        
+    def teardown_method(self):
+        pass
+
+    def test_w(self, qtbot):
+        pass
